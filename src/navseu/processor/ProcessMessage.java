117ca48d57467a4b3dd9e1eb467ca2729627c116
DNAKMEMES <DNAKMEMES@DESKTOP-KFKV977>
2016-01-23 20:58:43 -0600

    Successfully logs into an account, join and leave channels. Also Receives messages successfully using .ready();

---
+++ src/navseu/processor/ProcessMessage.java
@@ -0,0 +1,8 @@
+package navseu.processor;
+
+public class ProcessMessage {
+	public void recieveMessage(String message)
+	{
+		
+	}
+}
--- src/navseu/server/Client.java
+++ src/navseu/server/Client.java
@@ -1,10 +1,14 @@
 package navseu.server;
 
+
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 public class Client {
 	public String servername = "irc.twitch.tv";
 	public int port = 6667;
@@ -13,66 +17,133 @@ public class Client {
 	public String password = "oauth:68phvkvj3jiuxucmxgnblrkqvdgc4a";
 	public PrintWriter pw;
 	public BufferedReader br;
+	public BufferedReader fr;
 	public InputStreamReader isr;
+	public String message = "potato";
+	public ArrayList connectedChannels = new ArrayList<String>();
 	public Client()
 	{
 		
 		try
 		{
-			socket = new Socket(servername,port);
-			pw = new PrintWriter(socket.getOutputStream());
-			isr = new InputStreamReader(socket.getInputStream());
-			br = new BufferedReader(isr);
-			String message = br.readLine();
-			System.out.println("Server says: " + message);
-			sendMessage("PASS " + password);
-			sendMessage("NICK " + username);
 			
-			while(socket.isConnected() == true)
-			{
-				message = br.readLine();
-				System.out.println("Server sent " + message);
-				proccessMessage(message);
-			}
+			fr = new BufferedReader(new FileReader("C:/Users/DNAKMEMES/Documents/Twitch_Oauth.txt"));
+			password = fr.readLine();
+			System.out.println(password);
+			fr.close();
+			socket = new Socket(servername,port);
+			System.out.println(socket.isConnected());
+			pw = new PrintWriter(socket.getOutputStream());
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			
+			login();
+			System.out.println("Logged in");
+			
 		}
 		catch(IOException ioException)
 		{
 			System.out.println("Error");
 		}
-		finally
-		{
-			try
-			{
-				socket.close();
-			}
-			catch(IOException ioException)
-			{
-				ioException.printStackTrace();
-			}
-			
-		}
-		
 		
 	}
 	public void sendMessage(String mess)
 	{
-		try
-		{
-			mess = mess + "\n";
-			pw.print(mess);
-			pw.flush();
-			System.out.println("Sent message: " + mess);
-			String message = br.readLine();
-			System.out.println("Server says: " + message);
-		}
-		catch(IOException ioException)
-		{
-			System.out.println("Error");
-		}
+		mess = mess + "\n";
+		pw.print(mess);
+		pw.flush();
+		System.out.println("Sent message: " + mess);
 		
 	}
-	public void proccessMessage(String message)
+	public void processMessage(String message)
 	{
 		
 	}
+	public void login() throws IOException
+	{
+		sendMessage("PASS " + password);
+		sendMessage("NICK " + username);
+		message = br.readLine();
+		while(br.ready())
+		{
+			System.out.println(message);
+			message = br.readLine();
+		}
+	}
+	public void joinChannel(String channel)
+	{
+		sendMessage("JOIN #" + channel);
+		try
+		{
+			message = br.readLine();
+			while(br.ready())
+			{
+				System.out.println(message);
+				message = br.readLine();
+			}
+		}
+		catch(IOException ioException)
+		{
+			System.out.println("Error: " + ioException);
+		}
+		connectedChannels.add(channel);
+	}
+	public void leaveChannel(String channel)
+	{
+		sendMessage("PART #" + channel);
+		try
+		{
+			message = br.readLine();
+			while(br.ready())
+			{
+				System.out.println(message);
+				message = br.readLine();
+			}
+		}
+		catch(IOException ioException)
+		{
+			System.out.println("Error: " + ioException);
+		}
+		connectedChannels.remove(channel);
+	}
+	public void sendChat(String channel, String message)
+	{
+		String prefix = "PRIVMSG #";
+		if(connectedChannels.contains(channel))
+		{
+			prefix += channel;
+			prefix += " :";
+			String finalMessage = prefix + message;
+			sendMessage(finalMessage);
+		}
+		else
+		{
+			System.out.println("You are not connected to this chat");
+		}
+	}
+	public void reveiveMessages() throws IOException
+	{
+		while(br.ready())
+		{
+			message = br.readLine();
+			processMessage(message);
+			System.out.println(message);
+			
+		}
+	}
+	public void closeConnections()
+	{
+		try
+		{
+			br.close();
+			pw.close();
+			socket.close();
+		}
+		catch(IOException ioException)
+		{
+			ioException.printStackTrace();
+		}
+	}
+	
+	
 }
